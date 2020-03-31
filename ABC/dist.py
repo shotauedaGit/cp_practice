0f1a42ae9e1e00e78a272661d6049d00aa0cd2a0
shotauedaGit <shota.ueda.ak@gmail.com>
2020-03-31 16:55:31 +0900

    分別してくれるやつ追加

--- ABC/dist.py
+++ ABC/dist.py
@@ -1,10 +1,10 @@
 import os
 
-os.chdir(r"C:\prog\gitrepos\cp_practice\AGC")
+os.chdir(r"C:\prog\gitrepos\cp_practice\ABC")
 files = os.listdir()
 
 for file in files:
     l = len(file)
     ##print(file[l-3:])
-    if(file[:3] ==  "AGC" and file[l-3:]=="cpp"):
+    if(file[:3] ==  "ABC" and file[l-3:]=="cpp"):
         os.renames(file,file[:6]+"\\"+file)

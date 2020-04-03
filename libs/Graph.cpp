35320b7d2d5694172bd1e33b9b2cf60a6aabfc5c
shotauedaGit <shota.ueda.ak@gmail.com>
2020-04-03 17:12:47 +0900

    Graph.cpp（を作成しただけ）

---
+++ libs/Graph.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//要りそうなやつ
+#define se secondaa
+#define fi first
+#define all(a)  (a).begin(),(a).end()
+
+typedef long long ll;
+
+template<class T,class U>bool chmax(T &a, const U &b){if(a<b){a=b;return 1;}return 0;}
+template<class T,class U>bool chmin(T &a, const U &b){if(b<a){a=b;return 1;}return 0;}
+
+
+//ただの怠慢
+#define db(x) cerr<<#x<<" = "<<x<<" ";
+#define db2(x,y) cerr<<"("<<#x<<", "<<#y<<") = ("<<x<<", "<<y<<") ";
+#define db3(x,y,z) cerr<<"("<<#x<<", "<<#y<<", "<<#z<<") = ("<<x<<", "<<y<<", "<<z<<") ";
+#define ln cout<<endl;
+
+#define rep(i,n) for(int i=0,_i=(n);i<_i;++i)
+#define rep1(i,a,b) for(int a_=(a),b_=(b),i=a_;i<b_;++i)
+#define repr(i,n) for(int _i=(n),i=_i;i>0;--i)
+
+class einfo{
+
+    einfo();
+
+    int frm;
+    int to;
+    int cost;
+};
+
+class Graph{
+    public:
+
+    int Vmx=200010;
+    int Emx=200010;
+
+
+
+};

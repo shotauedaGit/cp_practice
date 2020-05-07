0d2fc72ae324646bf773ae6acf2bca125d6c4d61
shotauedaGit <shota.ueda.ak@gmail.com>
2020-05-07 16:31:32 +0900

    ABC57,58,と51D

---
+++ ABC/ABC057B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1001001001
+#define LINF 1001001001001001001
+#define MOD 1000000007
+#define MOD2 998244353
+
+template<class T,class U>bool chmax(T &a, const U &b){if(a<b){a=b;return 1;}return 0;}
+template<class T,class U>bool chmin(T &a, const U &b){if(b<a){a=b;return 1;}return 0;}
+
+#define rep(i,n) for(int i=0,_i=(n);i<_i;++i)
+#define rep1(i,a,b) for(int a_=(a),b_=(b),i=a_;i<b_;++i)
+#define repr(i,n) for(int _i=(n),i=_i;i>0;--i)
+
+#define db(x) cerr<<#x<<" = "<<x<<" ";
+#define db2(x,y) cerr<<"("<<#x<<", "<<#y<<") = ("<<x<<", "<<y<<") ";
+#define db3(x,y,z) cerr<<"("<<#x<<", "<<#y<<", "<<#z<<") = ("<<x<<", "<<y<<", "<<z<<") ";
+#define ln cout<<endl;
+
+#define all(a)  (a).begin(),(a).end()
+#define dig(n)  to_string(n).length()
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define se second
+#define fi first
+
+typedef long long ll;
+
+typedef pair<int,int> P;
+typedef pair<int,P> iP;
+typedef pair<P,P> PP;
+
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+int main(){
+
+    bool flag=false;
+
+    int n,m;
+    cin>>n>>m;
+
+    vector<P> st(n);
+    vector<int> ans(n,0);
+    vector<P> cp(m);
+
+    rep(i,n){
+        int a,b;
+        cin>>a>>b;
+        st[i] = P(a,b);
+    }
+
+    rep(i,m){
+        int a,b;
+        cin>>a>>b;
+        cp[i] = P(a,b);
+    }
+
+
+    rep(i,n){
+
+        int mnd = INF;
+
+        int a = st[i].fi;
+        int b = st[i].se;
+
+        rep(j,m){
+            int c = cp[j].fi;
+            int d = cp[j].se;
+
+            int dist  = abs(a-c)+abs(b-d);
+            if(chmin(mnd,dist))ans[i]=j+1;
+        }
+    }
+
+    for(int ai:ans)cout<<ai<<endl;
+
+    //cout <<fixed<<setprecision(16)<< << endl;
+
+    //if(flag)cout << "Yes" <<endl;
+    //else cout << "No" <<endl;
+
+    return 0;
+}
---
+++ ABC/ABC058C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1001001001
+#define LINF 1001001001001001001
+#define MOD 1000000007
+#define MOD2 998244353
+
+template<class T,class U>bool chmax(T &a, const U &b){if(a<b){a=b;return 1;}return 0;}
+template<class T,class U>bool chmin(T &a, const U &b){if(b<a){a=b;return 1;}return 0;}
+
+#define rep(i,n) for(int i=0,_i=(n);i<_i;++i)
+#define rep1(i,a,b) for(int a_=(a),b_=(b),i=a_;i<b_;++i)
+#define repr(i,n) for(int _i=(n),i=_i;i>0;--i)
+
+#define db(x) cerr<<#x<<" = "<<x<<" ";
+#define db2(x,y) cerr<<"("<<#x<<", "<<#y<<") = ("<<x<<", "<<y<<") ";
+#define db3(x,y,z) cerr<<"("<<#x<<", "<<#y<<", "<<#z<<") = ("<<x<<", "<<y<<", "<<z<<") ";
+#define ln cout<<endl;
+
+#define all(a)  (a).begin(),(a).end()
+#define dig(n)  to_string(n).length()
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define se second
+#define fi first
+
+typedef long long ll;
+
+typedef pair<int,int> P;
+typedef pair<int,P> iP;
+typedef pair<P,P> PP;
+
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+int main(){
+
+    bool flag=false;
+    ll ans=0,sum=0;
+
+    int n;
+    cin>>n;
+
+    map<string,int> subs;
+
+    vector<int> freq(26,100);
+
+    rep(i,n){
+        string si;
+        cin>>si;
+
+        vector<int> tmp(26,0);
+        
+        rep(j,si.length()){
+            tmp[ (si[j])-('a') ]++;
+        }
+
+        rep(j,26)chmin(freq[j],tmp[j]);
+    }
+
+    bool exAns=false;
+    rep(i,26)if(freq[i]>0)exAns=true;
+
+    string ansS="";
+    if(exAns){
+        rep(i,26)rep(j,freq[i]){
+            ansS += ('a'+i);
+        }
+    }
+    cout<<ansS<<endl;
+    //cout <<fixed<<setprecision(16)<< << endl;
+    //if(flag)cout << "Yes" <<endl;
+    //else cout << "No" <<endl;
+    return 0;
+}
